082f8edcf60cb447d62e87e6e1cd14457fd8b13b
Lissa692 <125963481+Lissa692@users.noreply.github.com>
2023-08-13 22:17:31 +0300

    commit

---
+++ src/index.js
@@ -0,0 +1,101 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import OnlyScroll from 'only-scrollbar';
+import { getFetchPhoto } from './pixabay-api';
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
+
+let fetchingInProgress = false; // Перевірка наявності активного запиту
+let currentSearchQuery = '';
+let page = 1;
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+function createTemplatePhoto(photos) {
+  const infoAboutPhoto = photos
+    .map(photo => {
+      return `
+        <a href="${photo.largeImageURL}" class="photo-card">
+        <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
+        <div class="info">
+        <p class="info-item"><b>Likes:</b> ${photo.likes}</p>
+        <p class="info-item"><b>Views:</b> ${photo.views}</p>
+        <p class="info-item"><b>Comments:</b> ${photo.comments}</p>
+        <p class="info-item"><b>Downloads:</b> ${photo.downloads}</p>
+        </div>
+        </a>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', infoAboutPhoto);
+  lightbox.refresh();
+}
+
+async function handleSearchFormSubmit(event) {
+  event.preventDefault();
+  page = 1;
+  gallery.innerHTML = '';
+
+  const formData = new FormData(event.currentTarget);
+  const searchQuery = formData.get('searchQuery').trim();
+
+  if (searchQuery === '') {
+    return;
+  }
+
+  currentSearchQuery = searchQuery;
+
+  try {
+    fetchingInProgress = true; // Позначаємо, що запит активний
+    const { hits, totalHits } = await getFetchPhoto(searchQuery, page);
+    if (hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    createTemplatePhoto(hits);
+
+    if (hits.length < totalHits) {
+      // document.querySelector('.load-more').style.display = 'block';
+    }
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+  } catch (error) {
+    Notiflix.Notify.failure('Error fetching images. Please try again later.');
+    console.error('Error fetching images:', error);
+  } finally {
+    fetchingInProgress = false; // Запит завершено, змінюємо стан на false
+  }
+}
+loadMore.addEventListener('click', loadMoreImages);
+
+async function loadMoreImages() {
+  page += 1;
+  try {
+    fetchingInProgress = true; // Позначаємо, що запит активний
+    const { hits, totalHits } = await getFetchPhoto(currentSearchQuery, page);
+    createTemplatePhoto(hits);
+    loadMore.disabled = true;
+
+    // Оновлюємо SimpleLightbox з новими зображеннями
+    lightbox.refresh();
+
+    const loadedImagesCount = (page - 1) * 40 + hits.length;
+    if (loadedImagesCount >= totalHits) {
+      // document.querySelector('.load-more').style.display = 'none';
+      loadMore.disabled = false;
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    Notiflix.Notify.failure('Error fetching images. Please try again later.');
+    console.error('Error fetching images:', error);
+  } finally {
+    fetchingInProgress = false; // Запит завершено, змінюємо стан на false
+  }
+}
+searchForm.addEventListener('submit', handleSearchFormSubmit);
---
+++ src/pixabay-api.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '38774663-b09d2594b348db9c881b41960';
+
+export async function getFetchPhoto(query, page = 1) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: page,
+      },
+    });
+
+    if (response.status >= 400) {
+      throw new Error('API error');
+    }
+
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching images:', error);
+  }
+}
+// export async function getFetchPhoto(searchQuery, page = 1) {
+//   try {
+//     const data = await axios.get(`${BASE_URL}`, {
+//       params: {
+//         key: API_KEY,
+//         q: searchQuery,
+//         image_type: photo,
+//         orientation: horizontal,
+//         safesearch: true,
+//         page: page,
+//         per_page: 40,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
